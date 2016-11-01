543f99a0504a75428f898fab08c30d856ab39bd4
Marcel Bonnet <marcelbonnet@machine.STUDIO>
2016-11-01 00:02:48 -0200

    remove stale script

--- mk_ardour_dist.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/local/bin/bash
-source ~/.bashrc 2&>/dev/null
-criardistfile `make -VPKGNAME` ~/devel/ports/src/ardour ~/devel/ports/distfiles/ && make DISTDIR=~/devel/ports/distfiles/ makesum
